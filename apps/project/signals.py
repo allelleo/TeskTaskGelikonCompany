f106f06908ab839454f73a9451465d7da5b2433b
allelleo <dev.allelleo@internet.ru>
2024-02-27 20:01:16 +0300

    bugfix

--- apps/project/signals.py
+++ apps/project/signals.py
@@ -7,10 +7,11 @@ from .models import Task
 @receiver(post_delete, sender=Task)
 def delete_task(sender, instance, using, **kwargs):
     print("task delete")
+    print(kwargs)
     print(instance)
     print(sender)
     executor = instance.executor
     executor.ended_tasks += 1
     executor.save()
-    instance.delete()
+    instance.delete(a=1)
     return True
