850b6ecc9d81d1346a70ad689630c77236e8d13c
allelleo <dev.allelleo@internet.ru>
2024-02-27 20:10:00 +0300

    admin delete task logic done

--- apps/project/signals.py
+++ apps/project/signals.py
@@ -6,11 +6,6 @@ from .models import Task
 
 @receiver(post_delete, sender=Task)
 def delete_task(sender, instance, **kwargs):
-
-    print("task delete")
-    print(kwargs)
-    print(instance)
     executor = instance.executor
     executor.ended_tasks += 1
     executor.save()
-    return True
