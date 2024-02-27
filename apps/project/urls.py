c528dd3f234218924cb20b2bafd817c2f5a2c580
allelleo <dev.allelleo@internet.ru>
2024-02-27 18:55:09 +0300

    fix

--- apps/project/urls.py
+++ apps/project/urls.py
@@ -5,5 +5,5 @@ from .views import TaskDetail, TaskList, delete_task
 urlpatterns = [
     path("", TaskList.as_view(), name="task-list"),
     path("<int:pk>/", TaskDetail.as_view(), name="task-detail"),
-    path("/destroy", delete_task),
+    path("destroy/", delete_task),
 ]
